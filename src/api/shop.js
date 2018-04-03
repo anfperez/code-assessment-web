d0140a2b2d6f10935de0651b44b9fc3755f349c7
Angela Perez <angelaperez@Angelas-MacBook-Pro.local>
2018-04-03 14:08:50 -0400

    testing witha new class called ProductTable inside shop.api

--- src/api/shop.js
+++ src/api/shop.js
@@ -1,11 +1,100 @@
 /**
  * Mocking client-server processing
  */
+ /*
 import React from 'react'
 
 const TIMEOUT = 100
 
 let products;
+class ProductApi extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+    };
+    this.getProducts = this.getProducts.bind(this)
+  }
+  componentDidMount() {
+  	/*
+    fetch('http://tech.work.co/shopping-cart/products.json')
+      .then(results => {
+        return results.json();
+      }).then(data => {
+
+    let products = []
+    for (let i =0; i < data.length; i++) {
+      products.push(data[i])
+      }
+    console.log("json looks like" + JSON.stringify(products))
+    
+   this.setState({products: products});
+
+    console.log(products) 
+  });
+  
+  fetch('http://tech.work.co/shopping-cart/products.json')
+      .then(results => {
+        return results.json();
+      }).then(data => {
+    
+   this.state.products = data;
+
+    console.log(products) 
+  });
+  }
+
+  render() {
+  	//getProducts()
+    return (
+      <div>
+        <div className="container2">
+          {this.state.products}
+        </div>
+      </div>
+      )
+    }
+}
+
+
+export default {
+  getProducts: (cb, timeout) => setTimeout(() => cb({ products }), timeout || TIMEOUT),
+  buyProducts: (payload, cb, timeout) => setTimeout(() => cb(), timeout || TIMEOUT)
+}
+*/
+import React from 'react'
+
+const TIMEOUT = 100
+
+let products;
+
+class ProductTable extends React.Component {
+  render() {
+    const rows = [];
+
+    this.props.products.forEach((product) => {
+      rows.push(
+      	<tr>
+	        <td>{product.productTitle}></td>
+	        <td>{product.price.value}></td>
+        </tr>
+        );
+    });
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+}
+
 class ProductApi extends React.Component {
   constructor() {
     super();
@@ -24,13 +113,11 @@ class ProductApi extends React.Component {
     for (let i =0; i < data.length; i++) {
       products.push(data[i])
       }
-    console.log("jsonproduct=" + JSON.stringify(products))
+    console.log("json looks like" + JSON.stringify(products))
 
-
-    
    this.setState({products: products});
 
-    console.log(products) 
+    console.log(products)
   });
   }
 
@@ -39,7 +126,7 @@ class ProductApi extends React.Component {
     return (
       <div>
         <div className="container2">
-          {this.state.products}
+        	<ProductTable products={this.state.products} />
         </div>
       </div>
       )
